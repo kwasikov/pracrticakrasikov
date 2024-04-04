7268f60e45251a96d6b6cc58757295dd85171d49
Fail <failkozyrev005@gmail.com>
2024-04-04 11:27:14 +0400

    practice day3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,27 +22,64 @@ namespace kras_lit
         public Window1()
         {
             InitializeComponent();
-        }        
+        }
 
         private void registr(object sender, MouseButtonEventArgs e)
         {
-            RegistrarionWindow regWindow = new ();           
+            RegistrarionWindow regWindow = new();
             regWindow.Show();
-            this.Close();         
+            this.Close();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var Login = login.Text;
-            var Password = pass.Text;
-            var context = new AppDbContext();
-            var user = context.Users.SingleOrDefault(x => x.login == Login && x.password == Password);
-            if (user is null)
-                {
-                MessageBox.Show("Неправильный логин и пароль");
-            }
-            MessageBox.Show("Вы успешно вошли в аккаунт");
 
-        } 
+            var Password = pass.Text;
+            var loginoremail = login.Text;
+
+            var context = new AppDbContext();
+            var user = context.Users.SingleOrDefault(x => (x.login == loginoremail || x.email == loginoremail) && x.password == Password);
+
+            if (user is null)
+            {
+                MessageBox.Show("Неправильный логин и пароль");
+                return;
+            }
+            else
+            {
+                welcome welcomeWindow = new();
+                welcomeWindow.Show();
+                this.Close();
+                MessageBox.Show($"Вы успешно вошли в аккаунт ваш логин {loginoremail}  и пароль {Password}");
+            }
+
+        }
+        double eyecheck = 1;
+        private void eyeopen_Click(object sender, RoutedEventArgs e)
+        {
+            eyecheck++;
+            if (eyecheck % 2 == 0)
+            {
+                pass.Text = passPassword.Password;
+                passPassword.Visibility = Visibility.Hidden;
+
+                eyeopen.Visibility = Visibility.Hidden;
+                eyeclose.Visibility = Visibility.Visible;
+
+            }
+        }
+        private void eyeclose_Click(object sender, RoutedEventArgs e)
+        {
+            eyecheck++;
+            if (eyecheck % 2 != 0)
+            {
+
+                passPassword.Password = pass.Text;
+                passPassword.Visibility = Visibility.Visible;
+
+                eyeclose.Visibility = Visibility.Hidden;
+                eyeopen.Visibility = Visibility.Visible;
+            }
+        }
     }
 }
--- RegistrarionWindow.xaml.cs
+++ RegistrarionWindow.xaml.cs
@@ -40,31 +40,32 @@ namespace kras_lit
         {
             //email
 
-            string pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+/.[a-zA-Z0-9-.]+$";
+            string pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
             return Regex.IsMatch(email, pattern);
         }
-        private bool isvalidpassword(string email)
+        private bool isvalidpassword(string pass1)
         {
             //password
              
             string symvol = "!@#$%^&*()_+";
-            return pass1.Any(x=> symvol.Contains(x));
+            return pass1.Any(x => symvol.Contains(x));
         }
 
         private void reg_Click(object sender, RoutedEventArgs e)
         {
             var newlogin = login.Text;
             var newemail = email.Text;
-            var newpassword = pass1.Text;
+            var newpassword = pass1Password.Password;
             var secondpassword = pass2.Text;
 
             var context = new AppDbContext();
 
             bool isvalid = true;
+
             //проверка почты
             if (!isvalidemail(newemail))
             {
-                email.Text = "Неккоректный email";
+                emailtext.Text = "Неккоректный email";
                 RED.BorderBrush = Brushes.Red;
                 isvalid = false;
                 return;
@@ -72,8 +73,39 @@ namespace kras_lit
             }
             else
             {
+                emailtext.Text = "";
                 RED.BorderBrush = Brushes.MediumPurple;
                 
+            }
+
+            //проверка пароля
+            if (newpassword.Length<8)
+            {
+                text1.Text = "Слабый пароль";
+                redpass1.BorderBrush = Brushes.Red;
+                isvalid = false;
+                return;
+            }
+            else
+            {
+                text1.Text = "";
+
+                redpass1.BorderBrush= Brushes.MediumPurple;
+            }
+
+
+            //совпадение паролей
+            if (newpassword != secondpassword)
+            {
+                text2.Text = "- пароли не совпадают";
+                redpass2.BorderBrush = Brushes.Red;
+                isvalid = false;
+                return ;
+            }
+            else
+            {
+                text2.Text = "";
+                redpass2.BorderBrush= Brushes.MediumPurple;
             }
 
             isvalid = true;
@@ -89,6 +121,40 @@ namespace kras_lit
             context.SaveChanges();
             MessageBox.Show("Добро пожаловать в систему");
         }
+        double eyecheck = 1;
         
+
+        private void eyeopen_Click(object sender, RoutedEventArgs e)
+        {
+            eyecheck++;
+            if (eyecheck %2 ==0)
+            {
+                pass1Box.Text = pass1Password.Password;
+                pass1Password.Visibility = Visibility.Hidden;
+
+                pass2.Text = pass2Password.Password;
+                pass2Password.Visibility = Visibility.Hidden;
+
+                eyeopen.Visibility = Visibility.Hidden;
+                eyeclose.Visibility = Visibility.Visible;
+
+            }
+        }
+        private void eyeclose_Click(object sender, RoutedEventArgs e)
+        {
+            eyecheck++;
+            if (eyecheck % 2 != 0)
+            {
+
+                pass1Password.Password = pass1Box.Text;
+                pass1Password.Visibility = Visibility.Visible;
+
+                pass2Password.Password = pass2.Text;
+                pass2Password.Visibility = Visibility.Visible;
+
+                eyeclose.Visibility = Visibility.Hidden;
+                eyeopen.Visibility = Visibility.Visible;
+            }
+        }
     }
 }
